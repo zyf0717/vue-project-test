0020d6ff78c32b197c62edc384dc22a46223a784
zyf0717 <zyf0717@gmail.com>
2024-10-03 11:19:36 +0800

    Debugging

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-project-test/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-project-test/' : '/'
 }
