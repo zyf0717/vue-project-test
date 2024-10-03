178a7185744bfd3e5b030f8ce73c8a50ecb70d12
Yifei Zheng <zyf0717@gmail.com>
2024-10-03 15:36:52 +0800

    Created .env files and updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/vue-project-test/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+
+  // Load environment variables based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL, // Use the loaded base URL from environment variables
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+});
