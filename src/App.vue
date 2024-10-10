731e507c0202c19970d2a7fb4883890b61ff7cd2
zyf0717 <zyf0717@gmail.com>
2024-10-10 14:16:08 +0800

    Testing filters on titanic dataset

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,65 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { ref, onMounted } from 'vue'
+import Papa from 'papaparse'
+
+const csvData = ref(null)
+
+async function fetchAndParseCSV() {
+  try {
+    const response = await fetch('https://calmcode.io/static/data/titanic.csv')
+    const csvText = await response.text()
+    Papa.parse(csvText, {
+      header: true,
+      complete: function(results) {
+        console.log(results.data)
+        csvData.value = results.data
+      }
+    })
+  } catch (error) {
+    console.error('Error fetching and parsing CSV:', error)
+  }
+}
+
+onMounted(() => {
+  fetchAndParseCSV()
+})
+
+let sexList = ['All', 'Female', 'Male', 'Other', 'Unknown']
+let sex = ref(null)
+let minAge = ref(0)
+let maxAge = ref(100)
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <label for="sex">Gender: </label>
+    <select v-model="sex" id="sex">
+      <option value="" disabled selected>Select sex</option>
+      <option v-for="s in sexList" :key="s" :value="s">{{ s }}</option>
+    </select>
+    <p>Selected sex: {{ sex || 'None' }}</p>
+  </div>
+  <br> <br> <br>
+  <div>
+    <label for="minAge">Min Age: {{ minAge }} </label>
+    <br>
+    <input type="range" v-model="minAge" id="minAge" min="0" max="100" step="1">
+    <br>
+    <label for="maxAge">Max Age: {{ maxAge }} </label>
+    <br>
+    <input type="range" v-model="maxAge" id="maxAge" min="0" max="100" step="1">
+    <p>Selected Age Range: {{ minAge }} - {{ maxAge }}</p>
+  </div>
+  <div id="app">
+    <h1>CSV Data</h1>
+    <br>
+    <div v-if="csvData">
+      <pre>{{ csvData }}</pre>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
